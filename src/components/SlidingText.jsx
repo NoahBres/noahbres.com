a1070ce2aa813a0d2b29f4699fff9587a9ec557e
Noah <noahbres@gmail.com>
2019-06-03 04:11:24 -0500

    Create SlidingText.jsx

---
+++ src/components/SlidingText.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+import { useTrail, animated } from "react-spring"
+
+import styled from "styled-components"
+
+const SlidingText = ({
+  text,
+  toggleOnClick = false,
+  style = "",
+  textSize = 80,
+  yShift = 20,
+  children,
+}) => {
+  const [toggle, setToggle] = useState(true)
+
+  const config = { mass: 20, tension: 2000, friction: 160 }
+
+  const trail = useTrail(children.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    y: toggle ? 0 : yShift,
+    height: toggle ? textSize : 0,
+    from: { opacity: 0, y: yShift, height: 0 },
+  })
+
+  const AnimatedText = styled(animated.div)`
+    ${style}
+  `
+
+  return (
+    <div
+      onClick={() => toggleOnClick && setToggle(toggle => !toggle)}
+      style={{ cursor: "pointer", userSelect: "none" }}
+    >
+      {trail.map(({ y, height, ...rest }, index) => (
+        <AnimatedText
+          key={children[index].props.children}
+          className=""
+          style={{
+            ...rest,
+            transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+          }}
+        >
+          <animated.div style={{ height, overflow: "hidden" }}>
+            {children[index]}
+          </animated.div>
+        </AnimatedText>
+      ))}
+    </div>
+  )
+}
+
+export default SlidingText
