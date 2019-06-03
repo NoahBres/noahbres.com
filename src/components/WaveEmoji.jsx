44e606c6d068c09cc5c3709dda50ad13d1a490ad
Noah <noahbres@gmail.com>
2019-06-03 05:03:33 -0500

    Add wave on hover to WaveEmoji

--- src/components/WaveEmoji.jsx
+++ src/components/WaveEmoji.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react"
+import { useSpring, animated } from "react-spring"
 
-const WaveEmoji = ({ fontSize = "0.7em" }) => {
+const WaveEmoji = ({ fontSize = "0.7em", style }) => {
   const [isHovered, setHover] = useState(false)
 
+  const config = { mass: 5, tension: 100, friction: 1 }
+  const { rotate } = useSpring({ rotate: isHovered ? 80 : 0, config })
+
   return (
-    <span style={{ fontSize }} role="img" aria-label="Wave Emoji">
+    <animated.span
+      role="img"
+      aria-label="Wave Emoji"
+      onMouseEnter={() => setHover(i => true)}
+      onMouseLeave={() => setHover(i => false)}
+      style={{
+        fontSize,
+        transform: rotate.interpolate(r => `rotate(${r}deg)`),
+        ...style,
+      }}
+    >
       👋
-    </span>
+    </animated.span>
   )
 }
 
