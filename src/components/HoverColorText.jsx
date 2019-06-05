d7c5c37add75b1601efb43ebaf509eb12844b60d
Noah <noahbres@gmail.com>
2019-06-04 23:44:51 -0500

    Implement HoverColorText color changing

--- src/components/HoverColorText.jsx
+++ src/components/HoverColorText.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 
 const HoverColorText = ({ text }) => {
+  const onHover = event => {
+    console.log(event.target)
+    event.target.style.color = `hsl(${Math.random() * 255}, ${Math.random() *
+      20 +
+      80}%, 92%)`
+  }
+
   return (
     <span>
-      {text.split("").map(char => (
-        <span>{char}</span>
+      {text.split(" ").map((word, i) => (
+        <span key={word + i}>
+          {word.split("").map((char, i) => (
+            <span key={char + i} onMouseEnter={onHover}>
+              {char}
+            </span>
+          ))}{" "}
+        </span>
       ))}
     </span>
   )
