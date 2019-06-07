2c744a45ea0b32e099f61f116d2c35d5e80a10d3
Noah <noahbres@gmail.com>
2019-06-07 02:03:05 -0500

    Add BackgroundBlobs to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroText from "../components/HeroText"
+import BackgroundBlobs from "../components/BackgroundBlobs"
 
 const Header = styled.section`
   display: flex;
@@ -14,14 +15,15 @@ const Header = styled.section`
   width: 100%;
   height: 100vh;
 
-  background: #21242d;
+  z-index: -1;
 `
 
 const IndexPage = () => {
   const parallaxRef = useRef()
 
   return (
-    <Layout>
+    // <Layout>
+    <>
       <SEO title="Noah Bresler" />
       <Parallax ref={parallaxRef} pages={2}>
         <ParallaxLayer offset={0} speed={0.5}>
@@ -30,7 +32,16 @@ const IndexPage = () => {
           </Header>
         </ParallaxLayer>
       </Parallax>
-    </Layout>
+      <BackgroundBlobs
+        styling={{
+          width: "100%",
+          height: "100%",
+          zIndex: "0",
+          background: "#21242d",
+        }}
+        blobs={3}
+      />
+    </>
   )
 }
 export default IndexPage
